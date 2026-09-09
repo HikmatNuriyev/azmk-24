41e156f03c594696788214ced26b5d255d74bc53
Hikmat Nuriyev <nuriyevhikmatt@gmail.com>
2026-09-09 15:23:02 +0400

    img problemi test 1

--- src/components/sections/management-page/managementData.js
+++ src/components/sections/management-page/managementData.js
@@ -3,14 +3,14 @@ export const managementData = [
     id: "lorem-ipsum-1",
     name: "Mübariz Qurbanov Əhməd oğlu",
     position: "Müşahidə Şurasının Sədri",
-    image: "/images/M_12.jpg",
+    image: "/images/mubariz.jpg",
     bio: "Mübariz Qurbanov 1976-cı ildə Şəki şəhərində anadan olub. 1993-1998-ci illərdə Azərbaycan Dövlət İqtisad Universitetinin Kommersiya fakültəsində Beynəlxalq İqtisadi Münasibətlər ixtisası üzrə təhsil alıb. Peşəkar fəaliyyətinə 2003-cü ildə başlayıb. 2003–2014-ci illərdə “İnkişaf üçün Maliyyə” Bank Olmayan Kredit Təşkilatında filial müdiri, Baş Kredit müdiri və İdarə Heyəti Sədrinin müavini vəzifələrində çalışıb.2014–2020-ci illərdə “Azərbaycan Beynəlxalq Bankı” ASC-də Pərakəndə Biznes Departamentində şöbə müdiri, departament direktorunun müavini kimi müxtəlif vəzifələrdə çalışaraq Bankın həyata keçirdiyi bir çox layihələrə rəhbərlik etmişdir. Mübariz Qurbanov “Frankfurt School of Finance and Management” və Stanford Univercity kimi nüfuzlu ali məktəblərin təşkil etdiyi müxtəlif sertifikasiya kurslarını uğurla bitirmişdir. 2025-ci ilin Sentyabr ayından etibarən “Azərbaycan Mikro Kredit” ASC-nin Müşahidə Şurasının sədri vəzifəsinə təyin edilmişdir. ",
   },
   {
     id: "lorem-ipsum-2",
     name: "Emil Muradov Gülbaba oğlu",
     position: "Direktor",
-    image: "/images/E2.jpg",
+    image: "/images/emin.JPG",
     bio: "Emil Muradov 1985-ci ildə Bakı şəhərində anadan olub. 2001–2005-ci illərdə Azərbaycan Dövlət İqtisad Universitetinin Ümumi iqtisadiyyat fakültəsində Maliyyə-kredit ixtisası üzrə təhsil alıb.Peşəkar fəaliyyətinə 2006-cı ildə bank sektorunda başlayıb. 2006–2007-ci illərdə “AccessBank” QSC-də, 2007–2012-ci illərdə “Bank Standard” KB QSC-də çalışıb.2012–2025-ci illərdə “Expressbank” ASC-də müxtəlif vəzifələrdə fəaliyyət göstərib. Bankda çalışdığı müddət ərzində pərakəndə bankçılıq, satış və xidmət sahələri üzrə peşəkar və idarəetmə təcrübəsi qazanıb. “Expressbank” ASC-də fəaliyyətini Pərakəndə bankçılıq departamentinin Satış və xidmətə dəstək şöbəsinin rəhbəri vəzifəsində tamamlayıb.2025-ci ildən etibarən “Azərbaycan Mikro Kredit” ASC-də Direktor vəzifəsində fəaliyyət göstərir.",
   },
 ];
