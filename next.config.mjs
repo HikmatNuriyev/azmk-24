a3ebe337241848b65f68bb19af58c5179e52f808
Hikmat Nuriyev <nuriyevhikmatt@gmail.com>
2026-09-14 15:48:56 +0400

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,18 @@
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'api.azmk.az',
-//       },
-//     ],
-//   },
-//   async redirects() {
-//     return [
-//       {
-//         source: '/index.php',
-//         destination: '/',
-//         permanent: true,
-//       },
-//     ];
-//   },
-// };
-
 
 // export default nextConfig;
-
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true, 
+  output: "export",
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'api.azmk.az',
+        protocol: "https",
+        hostname: "api.azmk.az",
       },
     ],
   },
